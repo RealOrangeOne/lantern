d73a6b7730187a13c4542304ee5567b8cf71ab35
Jake Howard <git@theorangeone.net>
2017-04-11 09:01:04 +0100

    Add basic 404 handler

--- lantern/main.py
+++ lantern/main.py
@@ -1,11 +1,17 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json, text
+from sanic.exceptions import NotFound
 
 app = Sanic()
 
 @app.route("/")
 async def test(request):
     return json({"hello": "world"})
+
+
+@app.exception(NotFound)
+def ignore_404s(request, exception):
+    return text("Page not found at {}".format(request.url), status=404)
 
 
 def main():
