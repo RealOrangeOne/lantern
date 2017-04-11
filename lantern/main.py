2b92acbc2a30648254581728ff3d7b4220bfda8f
Jake Howard <git@theorangeone.net>
2017-04-11 09:25:54 +0100

    Use multiple workers

--- lantern/main.py
+++ lantern/main.py
@@ -1,11 +1,12 @@
 from sanic import Sanic
 from lantern.route import main_route
-
+import os
 
 app = Sanic(__name__)
 
 
 def main():
+    workers = int(os.cpu_count()) or 1
     app.add_route(main_route, "/<path>")
     app.add_route(main_route, "/")
-    app.run(host="0.0.0.0", port="8000")
+    app.run(host="0.0.0.0", port=8000, workers=workers)
