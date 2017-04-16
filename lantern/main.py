ec4b40bb813781af91c8887d1ee69680ebab2233
Jake Howard <git@theorangeone.net>
2017-04-16 10:46:59 +0100

    Dont need so many workers

--- lantern/main.py
+++ lantern/main.py
@@ -2,7 +2,6 @@ from sanic import Sanic
 from sanic_useragent import SanicUserAgent
 from lantern.args import parse_args
 from lantern.route import build_main_route
-import os
 
 app = Sanic(__name__)
 SanicUserAgent.init_app(app)
@@ -10,8 +9,7 @@ SanicUserAgent.init_app(app)
 
 def main():
     args = parse_args()
-    workers = os.cpu_count() or 1
     main_route = build_main_route(args)
     app.add_route(main_route, "/<path>")
     app.add_route(main_route, "/")
-    app.run(host="0.0.0.0", port=args.port, workers=workers)
+    app.run(host="0.0.0.0", port=args.port)
